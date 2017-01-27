2a9836c50e1d99fd023306b3677a2c103faea08e
ellisthomas <ellisthomas@gmail.com>
2017-01-26 20:35:17 -0600

    for loop for grade F

--- grades.js
+++ grades.js
@@ -1,18 +1,30 @@
-// Loop over an array of student grades (values from 50-100) and outputs how many of each grades there are.
-
-// A score of 50-60 is an F
-// A score of 61-70 is a D
-// A score of 71-80 is a C
-// A score of 81-90 is a B
-// A score of 91-100 is an A
-
-// Start with array of random scores in your JavaScript
-
-// var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
-// Use console.log to output the following criteria:
-
-// 1. How many of each grade?
-// 2. What is the lowest grade?
-// 3. What is the highest grade?
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
 
 
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];  
+var scores = scores.sort(function(a,b) {
+	return a-b;
+}
+);
+
+
+
+console.log("scores", scores);
+
+var gradeA = 0;
+var gradeB = 0;
+var gradeC = 0;
+var gradeD = 0;
+var gradeF = 0;
+var lowestGrade = 0;
+var highestGrade = 0;
+
+var grade = [ ];
+
+for (var i = 0; i < scores.length; i++) {
+ if (scores[i] >= 50 && scores[i] <=60) {
+ 	grade.push("F");
+ 	
+ } 
+}
+console.log("grade", grade);
