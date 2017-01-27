2f93798d6187760d113524b6a263180ae5f67688
ellisthomas <ellisthomas@gmail.com>
2017-01-26 21:39:08 -0600

    js to get highest and lowest numbers

--- grades.js
+++ grades.js
@@ -37,9 +37,19 @@ for (var i = 0; i < scores.length; i++) {
 }  else if (scores[i] >=91 && scores[i] <= 100 ) {
 		grade.push ("A");
 		gradeA ++;
+ }
 }
 
+scores.min = function(scores) {
+	return Math.min.apply( Math, scores)
 }
+var lowestGrade = scores.min(scores)
+
+scores.max = function(scores) {
+	return Math.max.apply( Math, scores)
+}
+var highestGrade = scores.max(scores)
+
 
 console.log("grade", grade);
 console.log("Number of F's",gradeF);
@@ -47,6 +57,9 @@ console.log("Number of D's",gradeD);
 console.log("Number of C's",gradeC);
 console.log("Number of B's",gradeB);
 console.log("Number of A's",gradeA);
+console.log("Lowest grade", lowestGrade);
+console.log("Highest grade", highestGrade);
+
 
 
 
